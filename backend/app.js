38c3a9a6ecdf21e6c500aa499e2bc87ada0fc062
Ssamyyy <shauryasingh1040@gmail.com>
2025-06-16 12:16:35 +0530

    user auth done

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,14 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 
+const cookieParser = require('cookie-parser')
+
 const connectToDb = require('./db/db')
 connectToDb()
 
 const userRoutes = require('./routes/user.routes')
 
+app.use(cookieParser())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
